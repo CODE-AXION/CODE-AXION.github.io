edeae7a9b79a86b1b60b68eef4d9d4c3987a22ed
Suraj <surajvaghela7411@gmail.com>
2024-04-28 19:58:21 +0530

    added hashed router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ThemeProvider from './utils/ThemeContext';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -8,12 +8,12 @@ import store from './stores/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-    <Router basename="/CODE-AXION.github.io">
+    <HashRouter>
       <ThemeProvider>
       <Provider store={store}>
         <App />
         </Provider>,
       </ThemeProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
